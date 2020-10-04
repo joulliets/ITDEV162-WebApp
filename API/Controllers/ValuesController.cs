b174872426326a0b813ed84c0392a2e4001d81c6
Jouliet Morales <jouliet.25@gmai.com>
2020-10-04 14:24:52 -0500

    Adding values controller to the project.

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
