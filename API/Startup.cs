304b5bdc64707da6d77f9f665fa8f8ef3ef048b8
Jouliet Morales <jouliet.25@gmai.com>
2020-10-18 17:34:32 -0500

    Completed Activity 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,8 @@ namespace API
 {
     public class Startup
     {
+        private readonly string CorsPolicy = "CorsPolicy";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,13 +29,25 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy(name: CorsPolicy, 
+                                policyBuilder => 
+                                {
+                                    policyBuilder
+                                        .WithOrigins("http://localhost:3000")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                                });
+            });
+        
+            services.AddControllers();
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddDbContext<DataContext>(opt => 
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
-            //services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,16 +58,16 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseCors(CorsPolicy);
+            //app.UseHttpsRedirection();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+            
         }
     }
 }
