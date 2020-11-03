4dd3363356ef3d81ff2ae111c540ee369f568c27
Jouliet Morales <jouliet.25@gmai.com>
2020-11-03 11:19:47 -0600

    Completed Activity 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Posts;
 using Domain;
+using Persistence;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,12 +17,68 @@ namespace API.Controllers
     public class PostsController : ControllerBase
     {
         private readonly IMediator mediator;
+        private readonly DataContext context;
 
-        public PostsController(IMediator mediator) => this.mediator = mediator;
+        public PostsController(IMediator mediator, DataContext context)
+        {
+            this.mediator = mediator;
+            this.context = context;
+        }
 
         public async Task<ActionResult<List<Post>>> List()
         {
             return await this.mediator.Send(new List.Query());
         }
+
+        /// <summary>
+        /// GET api/posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
+        
+        [HttpGet]
+        public ActionResult<List<Post>> Get()
+        {
+            return this.context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            return this.context.Posts.Find(id);
+        }
+
+        /// <summary>
+        /// POST api/post
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns>A new post</returns>
+        [HttpPost]
+        public ActionResult<Post> Create([FromBody] Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+           
+            if (success)
+            {
+                return post;
+            }
+            throw new Exception("Error creating post");
+        }
+        
+
     }
 }
